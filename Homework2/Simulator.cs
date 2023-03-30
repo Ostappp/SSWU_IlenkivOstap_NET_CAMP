dfd61436a807892b8b3a2e4cb276fa474917d674
Ostap <ostapilienkiv@gmail.com>
2023-03-30 14:59:51 +0300

    Created project, described all classes

---
+++ Homework2/Simulator.cs
@@ -0,0 +1,29 @@
+﻿namespace Homework2
+{
+    internal class Simulator
+    {
+        List<User> users = new List<User>();//заповнити список корстувачів методом або створити конструктор
+        Pump waterTowerFiller = new Pump(1500);
+        Pump waterTowerTaker = new Pump();
+        public void Simulate(float source = 25000/*води у джерелі яке наповнює вежу*/)
+        {
+            WaterTower waterTower = new WaterTower(waterTowerFiller, waterTowerTaker);
+
+            ///цикл, який виконуватиметься або доки в source є вода, або доки в usersPump є юзери
+            ///якщо waterTowerFiller.IsOn рівне true 
+            ///     waterTowerFiller.IsOn присвоюємо !StopTakeWater
+            ///інакше
+            ///     waterTowerFiller.IsOn присвоюємо StartTakeWater
+            ///     
+            /// 
+            ///Виконуємо функцію waterTower.TakeWater 
+            ///для кожного користувача беремо воду
+            ///тобто для кожного виконуємо :
+            ///     якщо User.NeedWater()
+            ///         User.IncreaseWater(waterTower.GiveWater())
+            ///     інакше
+            ///         видаляємо користувача
+            ///
+        }
+    }
+}
