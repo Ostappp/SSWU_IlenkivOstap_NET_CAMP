017645e099a5815cc6c3e7b9154f94bbb9041229
Ostap <ostapilienkiv@gmail.com>
2023-04-16 03:21:26 +0300

    Project created. Task1 completed

---
+++ Homework5/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Homework5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {//Task1
+                Task1 garden1 = new Task1(50);
+                Task1 garden2 = new Task1(75);
+                Console.WriteLine($"First garden's fence length: {garden1.FenceLength}");
+                Console.WriteLine($"Second garden's fence length: {garden2.FenceLength}");
+
+                Console.WriteLine($"Is first garden's fence longer than second garden: {garden1 > garden2}");
+                List<(float, float)> treeInGarden = new List<(float, float)>
+                {
+                    (-1, 1 ),               (1, 1),
+                    (-1, 0 ),   (0, 0),     (1, 0),
+                    (-1, -1),               (1, -1),
+                };
+                Console.WriteLine($"{new Task1(treeInGarden).FenceLength}\n\n");
+                treeInGarden = new List<(float, float)>
+                {
+                    (-1, 1 ),
+                    (-1, 0 ),   (0, 0),     (1, 0),
+                    (-1, -1),   (0, -1),    (1, -1),
+                };
+                Console.WriteLine($"{new Task1(treeInGarden).FenceLength}\n\n");
+                treeInGarden = new List<(float, float)>
+                {
+                    (-1, 1 ),
+                    (-1, 0 ),   (0, 0),
+                    (-1, -1),   (0, -1),     (1, -1),
+                };
+                Console.WriteLine($"{new Task1(treeInGarden).FenceLength}\n\n");
+                treeInGarden = new List<(float, float)>
+                {
+                    (-1, 1 ),
+                                (0, 0),
+                    (-1, -1),               (1, -1),
+                };
+                Console.WriteLine($"{new Task1(treeInGarden).FenceLength}\n\n");
+                treeInGarden = new List<(float, float)>
+                {
+                                (0, 0),
+                    (-1, -1),               (1, -1),
+                };
+                Console.WriteLine($"{new Task1(treeInGarden).FenceLength}\n\n"); treeInGarden = new List<(float, float)>
+                {
+                    (-1, 1),                (1, 1),
+                                (0, 0),
+                    (-1, -1),               (1, -1),
+                };
+                Console.WriteLine($"{new Task1(treeInGarden).FenceLength}\n\n");
+                treeInGarden = new List<(float, float)>
+                {
+                                            (1, 1),
+                    (-1, 0 ),   (0, 0),     (1, 0),
+                    (-1, -1),               (1, -1),
+                };
+                Console.WriteLine($"{new Task1(treeInGarden).FenceLength}\n\n");
+                treeInGarden = new List<(float, float)>
+                {
+                    (-1, 1 ),
+                    (-1, 0 ),   (0, 0),
+                                (0, -1),     (1, -1),
+                };
+                Console.WriteLine($"{new Task1(treeInGarden).FenceLength}\n\n");
+            }
+
+
+        }
+    }
+}
