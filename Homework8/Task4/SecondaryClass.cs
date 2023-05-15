e75e3893ccf3f5d6633a8d947ae977bd0c344b9b
Ostap <ostapilienkiv@gmail.com>
2023-05-15 23:30:46 +0300

    Tasks completed

---
+++ Homework8/Task4/SecondaryClass.cs
@@ -0,0 +1,24 @@
+﻿namespace Homework8.Task4
+{
+    internal class SecondaryClass : BaseClass
+    {
+        string name;
+        public SecondaryClass(string name)
+        {
+            this.name = name;
+            //можна підписуватись та відписуватись
+            Event += Execution1;
+        }
+        /// <summary>
+        /// не можна викликати у лдочірніх класах івенти
+        /// </summary>
+        //public void InvokeDelegate()
+        //{
+        //    Delegate();
+        //}
+        string Execution1()
+        {
+            return $"{GetType().Name}\ttime: {DateTime.Now}";
+        }
+    }
+}
